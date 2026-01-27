edbeff87fefe997b0b8cc05d579f7047c72f1b1b
BurakHINGE <burakmentese16@gmail.com>
2026-01-27 19:41:23 +0300

    Turkish Version
    Code file has been identified as Turkish.

--- Codes/trReversedPyramid.java
+++ Codes/trReversedPyramid.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class reversedPyramid {
+public class trReversedPyramid {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
