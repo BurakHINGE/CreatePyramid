4370eee55f7bf51947998d5a5e3799c0203f51a5
BurakHINGE <burakmentese16@gmail.com>
2026-02-10 16:31:12 +0300

    Update trInvertedPyramid.java
    File name has been updated.

--- Codes/trInvertedPyramid.java
+++ Codes/trInvertedPyramid.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class trReversedPyramid {
+public class trInvertedPyramid {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
